68cfb5e6cd0568a617f5cfc1e7f3394607c82cad
liu <75195549+diyixin01@users.noreply.github.com>
2022-07-27 16:23:55 +0800

    Update ecdsa.py

--- ECDSA/ecdsa.py
+++ ECDSA/ecdsa.py
@@ -1,138 +1,174 @@
-#coding:utf-8
+from random import randint
+import math
+from gmpy2 import invert
+import random
+import binascii
+iv = "7380166F4914B2B9172442D7DA8A0600A96F30BC163138AAE38DEE4DB0FB0E4E"
+T = (0x79cc4519, 0x7a879d8a)
+index = "0123456789ABCDEF"
+W = []
+W_ = []
 
-#y^2=x^3+7
-A = 0
-B = 7
-
-# 有限域的阶
-P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
-# 椭圆曲线的阶
-N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+def LeftShift(num, left):
+    return (((num << left)&((1<<32)-1)) | (num >> (32 - left)))
 
 
-'''计算二次剩余 满足x^2=y mod p的x'''
-def Legendre(y,p): # 判断二次（非）剩余
-    return pow(y,(p - 1) // 2,p)
-def Tonelli_Shanks(y,p):
-    assert Legendre(y,p) == 1
-    if p % 4 == 3:
-        return pow(y,(p + 1) // 4,p)
-    q = p - 1
-    s = 0
-    while q % 2 == 0:
-        q = q // 2
-        s += 1
-    for z in range(2,p):
-        if Legendre(z,p) == p - 1:
-            c = pow(z,q,p)
-            break
-    r = pow(y,(q + 1) // 2,p)
-    t = pow(y,q,p)
-    m = s
-    if t % p == 1:
-        return r
+def Ti(x):
+    return (T[1]) if x > 15 else (T[0])
+
+
+def FFi(x, y, z, n):
+    return ((x & y) | (y & z) | (x & z)) if n > 15 else (x ^ y ^ z)
+
+
+def GGi(x, y, z, n):
+    return ((x & y) | ((~x) & z)) if n > 15 else (x ^ y ^ z)
+
+
+def P0(x):
+    return (x ^ LeftShift(x, 9) ^ LeftShift(x, 17))
+
+
+def P1(x):
+    return (x ^ LeftShift(x, 15) ^ LeftShift(x, 23))
+
+
+def padding(n, size,s):
+    s=list(s)
+    s.append('8')
+    for i in range(0, n // 4):
+        s.append("0")
+    s=''.join(s)
+    s+=hex(size)[2:].zfill(16).upper()
+    return n,s
+
+
+
+
+
+def Extend(B):
+    for i in range(0, 16):
+        W.append(int(B[(8 * i):(8 * i) + 8],16)%((1<<32)))
+
+    for i in range(16, 68):
+        W.append(int(hex((P1(W[i - 16] ^ W[i - 9] ^ LeftShift(W[i - 3], 15))) ^ (LeftShift(W[i - 13], 7) ^ W[i - 6])),16)%((1<<32)))
+
+    for i in range(0, 64):
+        W_.append(int(hex(W[i] ^ W[i + 4]),16)%((1<<32)))
+
+index=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+def uint_to_str(num,k = 8) :
+    s=""
+    for i in range(0,k):
+        s+=index[num % 16]
+        num//=16
+    return  s[::-1]
+
+def update(V, Bi):
+    temp = []
+    temp1 = []
+    for i in range(0, 8):
+        t="0x"+V[8 * i: (8 * i) + 8]
+        temp.append(int(t,16))
+        temp1.append(temp[i])
+    for i in range(0, 64):
+        SS1 = LeftShift((LeftShift(temp[0], 12) + temp[4] + LeftShift(Ti(i), i % 32))%(1<<32), 7)
+        SS2 = (SS1 ^ LeftShift(temp[0], 12))
+        t=(FFi(temp[0], temp[1], temp[2], i)+temp[3])%((1<<32))
+        TT1 = (FFi(temp[0], temp[1], temp[2], i) + temp[3] + SS2 + W_[i])%(1<<32)
+        TT2 = (GGi(temp[4], temp[5], temp[6], i) + temp[7] + SS1 + W[i])%(1<<32)
+        temp[3] = temp[2]
+        temp[2] = (LeftShift(temp[1], 9))
+        temp[1] = temp[0]
+        temp[0] = TT1
+        temp[7] = temp[6]
+        temp[6] = LeftShift(temp[5], 19)
+        temp[5] = temp[4]
+        temp[4] = P0(TT2)
+    result = ""
+    for i in range(0, 8):
+        result += uint_to_str(temp1[i] ^ temp[i])
+    return result.upper()
+
+
+def Hash(m):
+    size = len(m) * 4
+    num = (size + 1) % 512
+    t = 448 - num if num < 448 else 960 - num
+    k ,m= padding(t,size,m)
+    t=len(m)
+    group_number = (size + 65 + k) // 512
+    B = []
+    IV = []
+    IV.append(iv)
+    for i in range(0, group_number):
+        B.append(m[128 * i:128 * i + 128])
+        Extend(B[i])
+        IV.append(update(IV[i], B[i]))
+        W.clear()
+        W_.clear()
+    temp = IV[group_number]
+    return temp
+
+
+def add(x1,y1,x2,y2):
+    if x1==x2 and y1==p-y2:
+        return False
+    if x1!=x2:
+        lamda=((y2-y1)*invert(x2-x1, p))%p
     else:
-        i = 0
-        while t % p != 1: # 外层循环的判断条件
-            temp = pow(t,2**(i+1),p)
-            i += 1
-            if temp % p == 1:
-                b = pow(c,2**(m - i - 1),p)
-                r = r * b % p
-                c = b * b % p
-                t = t * c % p
-                m = i
-                i = 0 # 每次内层循环结束后i值要更新为0
-        return r
+        lamda=(((3*x1*x1+a)%p)*invert(2*y1, p))%p
+    x3=(lamda*lamda-x1-x2)%p
+    y3=(lamda*(x1-x3)-y1)%p
+    return x3,y3
+
+def mul_add(x,y,k):
+    k=bin(k)[2:]
+    qx,qy=x,y
+    for i in range(1,len(k)):
+        qx,qy=add(qx, qy, qx, qy)
+        if k[i]=='1':
+            qx,qy=add(qx, qy, x, y)
+    return qx,qy
+
+p=0x8542D69E4C044F18E8B92435BF6FF7DE457283915C45517D722EDB8B08F1DFC3    #256
+a=0x787968B4FA32C3FD2417842E73BBFEFF2F3C848B6831D7E0EC65228B3937E498
+b=0x63E4C6D3B23B0C849CF84241484BFE48F61D59A5B16BA06E6E12D1DA27C5249A
+n=0x8542D69E4C044F18E8B92435BF6FF7DD297720630485628D5AE74EE7C32E79B7
+Gx=0x421DEBD61B62EAB6746434EBC3CC315E32220B3BADD50BDC4C4E6C147FEDD43D
+Gy=0x0680512BCBB42C07D47349D2153B70C4E5D7FDFCBFA36EA1A85841B9E46E09A2
+P=(0x7e2c7fd68d3f5af1544f075ba4c29dd3e4cb7d1f96a880354875d9e7b6f24cec,
+   0x8058596a79762023a4dace0a107ae0b35ca448ebf2dd32cbf7a0fea3084d5233)
+da=0x68b5418eaaeeca62b41bf122e802b7919a8c7d385b62dea675a550d73824daa8
+
+def ECDSAsig(m):
+    d=randint(1,n)
+    x1,y1=mul_add(Gx,Gy,d)
+    r=x1%n
+    e=int(Hash(m),16)
+    s=(invert(d,n)*(e+da*r))%n
+    return hex(r),hex(s)
+
+def Verify(r,s,m):
+    if int(r,16) not in range(1,n-1):
+        return False
+    if int(s,16) not in range(1,n-1):
+        return False
+    e=int(Hash(m),16)
+    s=int(s,16)
+    r=int(r,16)
+    w=invert(s,n)
+    u1=(e*w)%n
+    u2=(r*w)%n
+    x1,y1=mul_add(Gx,Gy,u1)
+    x2,y2=mul_add(P[0],P[1],u2)
+    X=add(x1,y1,x2,y2)
+    if(X[0]%n==r):
+        return True
+    return False
 
 
-'''欧几里得求逆元'''
-def extended_euclidean_algorithm(j, k):
-    if j == k:
-        return (j, 1, 0)
-    else:
-        i = 0
-        j_array = [j]
-        k_array = [k]
-        q_array = []
-        r_array = []
-
-        prev_r_is_zero = False
-
-        while not (prev_r_is_zero):
-            q_array.append(k_array[i]//j_array[i])
-            r_array.append(k_array[i]%j_array[i])
-            k_array.append(j_array[i])
-            j_array.append(r_array[i])
-            i += 1
-            if r_array[i-1] == 0:
-                prev_r_is_zero = True
-        i -= 1
-        gcd = j_array[i]
-        x_array = [1]
-        y_array = [0]
-
-        i -= 1
-        total_steps = i
-
-        while i >= 0:
-            y_array.append(x_array[total_steps-i])
-            x_array.append(y_array[total_steps-i] - q_array[i]*x_array[total_steps-i])
-            i -= 1
-
-        return (gcd, x_array[-1], y_array[-1])
-
-def mod_inverse(j, n):
-    (gcd, x, y) = extended_euclidean_algorithm(j, n)
-
-    if gcd == 1:
-        return x%n
-    else:
-        return -1
-
-
-'''椭圆曲线加、乘'''
-def elliptic_add(p, q):
-    if p == 0 and q == 0: return 0
-    elif p == 0: return q
-    elif q == 0: return p
-    else:
-        # Swap p and q if px > qx.
-        if p[0] > q[0]:
-            temp = p
-            p = q
-            q = temp
-        r = []
-        slope = (q[1] - p[1])*mod_inverse(q[0] - p[0], P) % P
-
-        r.append((slope**2 - p[0] - q[0]) % P)
-        r.append((slope*(p[0] - r[0]) - p[1]) % P)
-
-        return (r[0], r[1])
-
-
-def elliptic_double(p):
-    r = []
-
-    slope = (3*p[0]**2 + A)*mod_inverse(2*p[1], P) % P
-
-    r.append((slope**2 - 2*p[0])%P)
-    r.append((slope*(p[0] - r[0]) - p[1])%P)
-
-    return (r[0], r[1])
-
-def elliptic_multiply(s, p):
-    n = p
-    r = 0 # 0 representing a point at infinity
-
-    s_binary = bin(s)[2:] # convert s to binary and remove the "0b" in the beginning
-    s_length = len(s_binary)
-
-    for i in reversed(range(s_length)):
-        if s_binary[i] == '1':
-            r = elliptic_add(r, n)
-        n = elliptic_double(n)
-
-    return r
-
-
+m="encryption standard"
+m=hex(int(binascii.b2a_hex(m.encode()).decode(),16)).upper()[2:]
+r,s=ECDSAsig(m)
+print(Verify(r,s,m))
