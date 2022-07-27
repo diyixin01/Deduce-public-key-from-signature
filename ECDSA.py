c959993cbf4b8a2f2a92274a78d04b375a0e9dc7
liu <75195549+diyixin01@users.noreply.github.com>
2022-07-27 17:15:08 +0800

    Update ECDSA.py

--- ECDSA.py
+++ ECDSA.py
@@ -1,177 +1,178 @@
-from random import randint
-import math
-from gmpy2 import invert
-import random
-import binascii
-iv = "7380166F4914B2B9172442D7DA8A0600A96F30BC163138AAE38DEE4DB0FB0E4E"
-T = (0x79cc4519, 0x7a879d8a)
-index = "0123456789ABCDEF"
-W = []
-W_ = []
+import secrets
 
-def LeftShift(num, left):
-    return (((num << left)&((1<<32)-1)) | (num >> (32 - left)))
+def boolQR(n, p):
+    return pow(n, (p - 1) // 2, p)
 
-
-def Ti(x):
-    return (T[1]) if x > 15 else (T[0])
-
-
-def FFi(x, y, z, n):
-    return ((x & y) | (y & z) | (x & z)) if n > 15 else (x ^ y ^ z)
-
-
-def GGi(x, y, z, n):
-    return ((x & y) | ((~x) & z)) if n > 15 else (x ^ y ^ z)
-
-
-def P0(x):
-    return (x ^ LeftShift(x, 9) ^ LeftShift(x, 17))
-
-
-def P1(x):
-    return (x ^ LeftShift(x, 15) ^ LeftShift(x, 23))
-
-
-def padding(n, size,s):
-    s=list(s)
-    s.append('8')
-    for i in range(0, n // 4):
-        s.append("0")
-    s=''.join(s)
-    s+=hex(size)[2:].zfill(16).upper()
-    return n,s
-
-
-
-
-
-def Extend(B):
-    for i in range(0, 16):
-        W.append(int(B[(8 * i):(8 * i) + 8],16)%((1<<32)))
-
-    for i in range(16, 68):
-        W.append(int(hex((P1(W[i - 16] ^ W[i - 9] ^ LeftShift(W[i - 3], 15))) ^ (LeftShift(W[i - 13], 7) ^ W[i - 6])),16)%((1<<32)))
-
-    for i in range(0, 64):
-        W_.append(int(hex(W[i] ^ W[i + 4]),16)%((1<<32)))
-
-index=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-def uint_to_str(num,k = 8) :
-    s=""
-    for i in range(0,k):
-        s+=index[num % 16]
-        num//=16
-    return  s[::-1]
-
-def update(V, Bi):
-    temp = []
-    temp1 = []
-    for i in range(0, 8):
-        t="0x"+V[8 * i: (8 * i) + 8]
-        temp.append(int(t,16))
-        temp1.append(temp[i])
-    for i in range(0, 64):
-        SS1 = LeftShift((LeftShift(temp[0], 12) + temp[4] + LeftShift(Ti(i), i % 32))%(1<<32), 7)
-        SS2 = (SS1 ^ LeftShift(temp[0], 12))
-        t=(FFi(temp[0], temp[1], temp[2], i)+temp[3])%((1<<32))
-        TT1 = (FFi(temp[0], temp[1], temp[2], i) + temp[3] + SS2 + W_[i])%(1<<32)
-        TT2 = (GGi(temp[4], temp[5], temp[6], i) + temp[7] + SS1 + W[i])%(1<<32)
-        temp[3] = temp[2]
-        temp[2] = (LeftShift(temp[1], 9))
-        temp[1] = temp[0]
-        temp[0] = TT1
-        temp[7] = temp[6]
-        temp[6] = LeftShift(temp[5], 19)
-        temp[5] = temp[4]
-        temp[4] = P0(TT2)
-    result = ""
-    for i in range(0, 8):
-        result += uint_to_str(temp1[i] ^ temp[i])
-    return result.upper()
-
-
-def Hash(m):
-    size = len(m) * 4
-    num = (size + 1) % 512
-    t = 448 - num if num < 448 else 960 - num
-    k ,m= padding(t,size,m)
-    t=len(m)
-    group_number = (size + 65 + k) // 512
-    B = []
-    IV = []
-    IV.append(iv)
-    for i in range(0, group_number):
-        B.append(m[128 * i:128 * i + 128])
-        Extend(B[i])
-        IV.append(update(IV[i], B[i]))
-        W.clear()
-        W_.clear()
-    temp = IV[group_number]
-    return temp
-
-
-def add(x1,y1,x2,y2):
-    if x1==x2 and y1==p-y2:
-        return False
-    if x1!=x2:
-        lamda=((y2-y1)*invert(x2-x1, p))%p
+def solveQR(n, p):
+    assert boolQR(n, p) == 1
+    if p % 4 == 3:
+        return pow(n, (p + 1) // 4, p)
+    q = p - 1
+    s = 0
+    while q % 2 == 0:
+        q = q // 2
+        s += 1
+    for z in range(2, p):
+        if boolQR(z, p) == p - 1:
+            c = pow(z, q, p)
+            break
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
+    m = s
+    if t % p == 1:
+        return r
     else:
-        lamda=(((3*x1*x1+a)%p)*invert(2*y1, p))%p
-    x3=(lamda*lamda-x1-x2)%p
-    y3=(lamda*(x1-x3)-y1)%p
-    return x3,y3
+        i = 0
+        while t % p != 1:
+            temp = pow(t, 2 ** (i + 1), p)
+            i += 1
+            if temp % p == 1:
+                b = pow(c, 2 ** (m - i - 1), p)
+                r = r * b % p
+                c = b * b % p
+                t = t * c % p
+                m = i
+                i = 0
+        return r
 
-def mul_add(x,y,k):
-    k=bin(k)[2:]
-    qx,qy=x,y
-    for i in range(1,len(k)):
-        qx,qy=add(qx, qy, qx, qy)
-        if k[i]=='1':
-            qx,qy=add(qx, qy, x, y)
-    return qx,qy
+def mod_invese(B, N):
+    if B == N:
+        return (B, 1, 0)
+    else:
+        i = 0
+        b = [B]
+        n = [N]
+        q = []
+        r = []
+        flag = False
 
-p=0x8542D69E4C044F18E8B92435BF6FF7DE457283915C45517D722EDB8B08F1DFC3    #256
-a=0x787968B4FA32C3FD2417842E73BBFEFF2F3C848B6831D7E0EC65228B3937E498
-b=0x63E4C6D3B23B0C849CF84241484BFE48F61D59A5B16BA06E6E12D1DA27C5249A
-n=0x8542D69E4C044F18E8B92435BF6FF7DD297720630485628D5AE74EE7C32E79B7
-Gx=0x421DEBD61B62EAB6746434EBC3CC315E32220B3BADD50BDC4C4E6C147FEDD43D
-Gy=0x0680512BCBB42C07D47349D2153B70C4E5D7FDFCBFA36EA1A85841B9E46E09A2
-P=(0x7e2c7fd68d3f5af1544f075ba4c29dd3e4cb7d1f96a880354875d9e7b6f24cec,
-   0x8058596a79762023a4dace0a107ae0b35ca448ebf2dd32cbf7a0fea3084d5233)
-da=0x68b5418eaaeeca62b41bf122e802b7919a8c7d385b62dea675a550d73824daa8
+        while not flag:
+            q.append(n[i] // b[i])
+            r.append(n[i] % b[i])
+            n.append(b[i])
+            b.append(r[i])
+            if r[i] == 0:
+                flag = True
+            i += 1
+        tmp = b[i - 1]
+        x = [1]
+        y = [0]
 
-def ECDSAsig(m):
-    d=randint(1,n)
-    x1,y1=mul_add(Gx,Gy,d)
-    r=x1%n
-    e=int(Hash(m),16)
-    s=(invert(d,n)*(e+da*r))%n
-    return hex(r),hex(s)
+        i -= 2;
+        num = i
 
-def Verify(r,s,m):
-    if int(r,16) not in range(1,n-1):
-        return False
-    if int(s,16) not in range(1,n-1):
-        return False
-    e=int(Hash(m),16)
-    s=int(s,16)
-    r=int(r,16)
-    w=invert(s,n)
-    u1=(e*w)%n
-    u2=(r*w)%n
-    x1,y1=mul_add(Gx,Gy,u1)
-    x2,y2=mul_add(P[0],P[1],u2)
-    X=add(x1,y1,x2,y2)
-    if(X[0]%n==r):
-        return True
-    return False
+        while i >= 0:
+            y.append(x[num - i])
+            x.append(y[num - i] - q[i] * x[num - i])
+            i -= 1
+
+        return (tmp, x[-1], y[-1])
 
 
-m="202000460082"
-print(m)
-m=hex(int(binascii.b2a_hex(m.encode()).decode(),16)).upper()[2:]
-r,s=ECDSAsig(m)
-print(r)
-print(s)
-print(Verify(r,s,m))
+def xgcd(b, n):
+    (g, x, y) = mod_invese(b, n)
+
+    if g == 1:
+        return x % n
+    else:
+        return -1
+
+
+a = 0
+b = 7
+p = 115792089237316195423570985008687907853269984665640564039457584007908834671663
+n = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+x = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+G = (x, y)
+
+def add(P, Q):  # P!=Q
+    if P == 0 and Q == 0:
+        return 0
+    elif P == 0:
+        return Q
+    elif Q == 0:
+        return P
+    else:
+        if P[0] > Q[0]:
+            tmp = P
+            P = Q
+            Q = tmp
+
+        Z = []
+        t = (Q[1] - P[1]) * xgcd(Q[0] - P[0], p) % p
+        Z.append((t ** 2 - P[0] - Q[0]) % p)
+        Z.append((t * (P[0] - Z[0]) - P[1]) % p)
+        return (Z[0], Z[1])
+
+
+def epoint_add1(P):  # P==Q
+    Z = []
+    tmp = (3 * P[0] ** 2 + a) * xgcd(2 * P[1], p) % p
+    Z.append((tmp ** 2 - 2 * P[0]) % p)
+    Z.append((tmp * (P[0] - Z[0]) - P[1]) % p)
+    return (Z[0], Z[1])
+
+
+# 椭圆曲线上的点乘
+def multi(k, g):
+    tmp = g
+    z = 0
+    k_bin = bin(k)[2:]
+    k_len = len(k_bin)
+
+    for i in reversed(range(k_len)):
+        if k_bin[i] == '1':
+            z = add(z, tmp)
+        tmp = epoint_add1(tmp)
+
+    return z
+
+
+def keygen():
+    sk = int(secrets.token_hex(32), 16)
+    pk = multi(sk, G)
+    return sk, pk
+
+
+
+def signature(sk, m):
+    e = hash(m)
+    k = secrets.randbelow(p)  # (0,p)的随机数
+    R = multi(k, G)  # P点=k*G
+    r = R[0] % p
+    s = xgcd(k, n) * (e + r * sk) % n
+
+    return (r, s)  # 以元组形式存在的签名
+
+def deduce(sign, m):
+    r = sign[0]
+    s = sign[1]
+    x = r % p
+    y = solveQR(((x ** 3) + 7), p)
+    e = hash(m)
+
+    P1 = (x, y)
+    P2 = (x, p - y)
+    sk1 = multi(s % n, P1)
+    tmp = multi(e % n, G)
+    tmp_i = (tmp[0], p - tmp[1])
+    tmp_1 = add(sk1, tmp_i)
+    pk1 = multi(xgcd(r, n), tmp_1)
+
+    sk2 = multi(s % n, P2)
+    tmp_2 = add(sk2, tmp_i)
+    pk2 = multi(xgcd(r, n), tmp_2)
+    return pk1, pk2
+
+
+if __name__ == '__main__':
+    sk, pk = keygen()
+    print("Public Key: \n", pk)
+    m = "hello,world!"
+    s = signature(sk, m)
+    print("\nSignature:\n ", s)
+    pub1, pub2 = deduce(s, m)
+    print('\nTry to deduce publickey from signature：')
+    print('\nPossible publickey_1：\n', pub1)
+    print('\nPossible publickey_2：\n', pub2)
